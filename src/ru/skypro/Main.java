cc90071cb603ee5d23beb9fe0805fbfc16e06b9d
TikhomirovaElvira <elviratikhomirova@mail.ru>
2022-04-15 19:20:43 +0300

    дз7

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -3,6 +3,24 @@ package ru.skypro;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// Задание 1
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.print("ФИО сотрудника - " + fullName);
+
+        // Задание 2
+        System.out.println();
+        String full = fullName.toUpperCase();
+        System.out.print("Данные ФИО сотрудника для заполнения отчета - " + full);
+
+        //Задание 3
+        System.out.println();
+        String fullName1 = "Иванов Семён Семёнович";
+        fullName1 = fullName1.replace("ё", "е");
+        System.out.print("Данные ФИО сотрудника - " + fullName1);
+
+
     }
 }
